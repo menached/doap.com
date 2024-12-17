0142182b992491aa477455826e207997e679aece
David Menache <dave@doap.com>
2024-12-16 19:42:48 -0600

    Deploy cart.js to the cloud.

--- cart.js
+++ cart.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // Update logo link
-    const logoLink = document.querySelector(".header a");
+    const logoLink = document.querySelector(".header a, .header > div > a, .header > a");
     if (logoLink) {
         logoLink.href = `https://${domainName}.doap.com/cart.php`;
         logoLink.title = `Call ${cityName} Doap!`;
