421923fb29370077eb01169da376b3e8a79cf0ec
David Menache <dave@doap.com>
2024-12-17 09:27:31 -0600

    Deploy cart.php to the cloud.

--- cart.php
+++ cart.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <script src="cart.js" defer></script>
+    <script src="zipFinder.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include('og_meta.php'); ?>
 </head>
