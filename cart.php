6c3b66f50995f5ba8275d9d9c8747125e5e22de7
David Menache <dave@doap.com>
2024-12-18 09:21:44 -0600

    Deploy cart.php to the cloud.

--- cart.php
+++ cart.php
@@ -218,6 +218,11 @@
                 <p>After placing your order, please check your email for further instructions on how to complete your payment.  Feel free to call us at (833)289-3627 for assistance. We're standing by to help!</p>
             </div>
         </div>
+    </form>
+
+    <div id="popupMessage" class="popup hidden">
+        <p id="popupText">Order Processed Successfully!</p>
+    </div>
     <!-- Footer Section -->
     <footer style="
         background-color: #f8f9fa;
@@ -246,11 +251,6 @@
     <script>
         document.getElementById("year").textContent = new Date().getFullYear();
     </script>
-    </form>
-
-    <div id="popupMessage" class="popup hidden">
-        <p id="popupText">Order Processed Successfully!</p>
-    </div>
 
 </body>
 </html>
