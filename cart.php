145e3288fc47c992ecd5234a6eaf05af65887483
David Menache <dave@doap.com>
2024-12-19 13:41:27 -0600

    Deploy cart.php to the cloud.

--- cart.php
+++ cart.php
@@ -200,6 +200,8 @@
                 <h3><i class="fas fa-phone-alt"></i> Need Assistance?</h3>
                 <p>After placing your order, please check your email for further instructions on how to complete your payment.  Feel free to call us at (833)289-3627 for assistance. We're standing by to help!</p>
             </div>
+<!-- Footer Section -->
+<?php include('footer.html'); ?>
         </div>
 
 
@@ -214,8 +216,6 @@
         <p id="popupText">Order Processed Successfully!</p>
     </div>
         
-<!-- Footer Section -->
-<?php include('footer.html'); ?>
 
 </body>
 </html>
