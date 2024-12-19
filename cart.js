167c08f9c0d481090c7e0f33751483b6bad946e1
David Menache <dave@doap.com>
2024-12-19 13:45:59 -0600

    Deploy cart.js to the cloud.

--- cart.js
+++ cart.js
@@ -2,6 +2,26 @@ console.log("cart.js started loading");
 document.addEventListener("DOMContentLoaded", () => {
 
 
+    // Get the modal
+    const modal = document.getElementById("imageModal");
+    const modalImage = document.getElementById("modalImage");
+    const captionText = document.getElementById("caption");
+
+    // Add event listener to all product images
+    document.querySelectorAll(".item img").forEach(img => {
+        img.addEventListener("click", function () {
+            modal.style.display = "block";
+            modalImage.src = this.src;
+            captionText.innerHTML = this.alt;
+        });
+    });
+
+    // Close the modal
+    window.closeModal = function () {
+        modal.style.display = "none";
+    };
+
+
     // Map subdomains to minimum order amounts
     const areaMinimum = {
         alamo: 40,
