9d935226dcc501977f953b91ce2dcd1ea895a050
David Menache <dave@doap.com>
2024-12-19 18:37:56 -0600

    Deploy head.php to the cloud.

---
+++ head.php
@@ -0,0 +1,23 @@
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP7XRHB9TQ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-VP7XRHB9TQ');
+    </script>
+    <script>
+        const hostname = window.location.hostname;
+        const subdomain = hostname.split('.')[0];
+    </script>
+        <title>Doap</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="cart.js" defer></script>
+    <script src="zipFinder.js" defer></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include('og_meta.php'); ?>
+
