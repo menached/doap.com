651133922e9c8f346d1c76af13bd4dcf6734b727
David Menache <dave@doap.com>
2024-12-19 19:14:52 -0600

    Deploy head.php to the cloud.

--- head.php
+++ head.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
 
     <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="doap_styles.css">
+    <link rel="stylesheet" href="styles.css">
     <script src="cart.js" defer></script>
     <script src="zipFinder.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
