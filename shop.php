4d92681c8f9e759135a0955f56fcc27105c1ac95
David Menache <dave@doap.com>
2024-12-21 14:38:37 -0600

    Deploy shop.php to the cloud.

--- shop.php
+++ shop.php
@@ -18,9 +18,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
 
     <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-    <script src="cart.js" defer></script>
-    <script src="zipFinder.js" defer></script>
+    <link rel="stylesheet" href="shopstyles.css">
+    <script src="shop.js" defer></script>
+    <script src="shopzipFinder.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include('og_meta.php'); ?>
 </head>
