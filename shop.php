c7d25a8f41c1a98ba265b27de35841c294a022e9
David Menache <dave@doap.com>
2024-12-21 17:02:20 -0600

    Deploy shop.php to the cloud.

--- shop.php
+++ shop.php
@@ -28,14 +28,14 @@
     <!-- Header Section -->
         <div class="header">
             <div class="logo-title-container">
-                <a href="/cart.php" target="_SELF"> <img src="https://www.doap.com/doap-logo-wording.png" alt="Doap Logo" class="doap-logo"> </a>
+                <a href="/shop.php" target="_SELF"> <img src="https://www.doap.com/doap-logo-wording.png" alt="Doap Logo" class="doap-logo"> </a>
                 <div class="text-container">
-                    <a class="nodec" href="/cart.php" target="_SELF"> <h1 id="cityName">Doap</h1> </a>
+                    <a class="nodec" href="/shop.php" target="_SELF"> <h1 id="cityName">Doap</h1> </a>
                     <a href="tel:8332893627" class="phone-number nodec">(833) 289-3627</a>
                 </div>
             </div>
             <div class="menu-title nodec">
-                    <a style="text-decoration:none!important;" class="nodec" href="/cart.php" target="_SELF">
+                    <a style="text-decoration:none!important;" class="nodec" href="/shop.php" target="_SELF">
                         <h2 class="menu-title-text nodec">Delivering Organic Awesome Pot</h2>
                     </a>
             </div>
@@ -51,7 +51,7 @@
 
     <form id="cartForm">
 
-    <?php include('products.html'); ?>
+    <?php include('shopproducts.html'); ?>
 
         <?php //include('flower.html'); ?>
         
