d306b16c3637c5131fae17d09545246ddeb6950d
David Menache <dave@doap.com>
2024-12-21 17:21:22 -0600

    Deploy shop.php to the cloud.

--- shop.php
+++ shop.php
@@ -1,28 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP7XRHB9TQ"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-VP7XRHB9TQ');
-    </script>
     <script>
         const hostname = window.location.hostname;
         const subdomain = hostname.split('.')[0];
     </script>
         <title>Doap</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
-
     <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="shopstyles.css">
     <script src="shop.js" defer></script>
     <script src="shopzipFinder.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php include('og_meta.php'); ?>
+    <?php //include('og_meta.php'); ?>
 </head>
 <body>
     <!-- Header Section -->
@@ -51,7 +41,7 @@
 
     <form id="cartForm">
 
-    <?php include('shopproducts.html'); ?>
+    <?php //include('shopproducts.html'); ?>
 
         <?php //include('flower.html'); ?>
         
