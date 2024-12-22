6417d44b5206d26e2a9ca75d51d81368adc43d05
David Menache <dave@doap.com>
2024-12-22 17:25:26 -0600

    Deploy head.php to the cloud.

--- head.php
+++ head.php
@@ -1,40 +1,22 @@
 <title>Doap</title>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VP7XRHB9TQ"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP7XRHB9TQ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-    gtag('config', 'G-VP7XRHB9TQ');
-</script>
-
-<script>
-    // Get the subdomain from the hostname
-    const hostname = window.location.hostname;
-    const subdomain = hostname.split('.')[0];
-
-    // Replace placeholders or static URLs dynamically
-    const styleLink = document.createElement("link");
-    styleLink.rel = "stylesheet";
-    styleLink.href = `https://${subdomain}.doap.com/shopstyles.css`;
-    document.head.appendChild(styleLink);
-
-    const shopScript = document.createElement("script");
-    shopScript.src = `https://${subdomain}.doap.com/shop.js`;
-    shopScript.type = "module";
-    shopScript.defer = true;
-    document.head.appendChild(shopScript);
-
-    const zipFinderScript = document.createElement("script");
-    zipFinderScript.src = `https://${subdomain}.doap.com/shopzipFinder.js`;
-    zipFinderScript.defer = true;
-    document.head.appendChild(zipFinderScript);
-
-    console.log(`Using resources for subdomain: ${subdomain}`);
-</script>
-
-<link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php include('og_meta.php'); ?>
-
+      gtag('config', 'G-VP7XRHB9TQ');
+    </script>
+    <script>
+        const hostname = window.location.hostname;
+        const subdomain = hostname.split('.')[0];
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="shopstyles.css">
+    <script src="shop.js" type="module" defer></script>
+    <script src="subdomainData.js" type="module" defer></script>
+    <script src="shopzipFinder.js" defer></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include('og_meta.php'); ?>
