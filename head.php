63f643e3a4bdabf578fdcf2f5eb6c326f2e32015
David Menache <dave@doap.com>
2024-12-21 21:00:46 -0600

    updates

--- head.php
+++ head.php
@@ -1,14 +1,13 @@
+<title>Doap</title>
     <script>
         const hostname = window.location.hostname;
         const subdomain = hostname.split('.')[0];
     </script>
-        <title>Doap</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" crossorigin="anonymous">
-
     <link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
-    <script src="doap.js" type="module" defer></script>
+    <link rel="stylesheet" href="shopstyles.css">
+    <script src="shop.js" type="module" defer></script>
     <script src="subdomainData.js" type="module" defer></script>
+    <script src="shopzipFinder.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include('og_meta.php'); ?>
-
