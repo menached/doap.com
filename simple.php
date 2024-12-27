6da35022dd54567e8334ddc37e04b197cfbff27f
David Menache <dave@doap.com>
2024-12-27 06:28:40 -0600

    simple

---
+++ simple.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include 'inc/php/head.php'; ?>
+    </head>
+    <body>
+        <div class="page-wrapper">
+            <div class="main-content">
+                <?php include 'inc/html/header.html'; ?>
+
+
+                <?php include 'inc/php/shop.php'; ?>
+
+
+            </div>
+            <?php include 'inc/html/footer.html'; ?>
+        </div>
+    </body>
+</html>
+
