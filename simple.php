e70d1c330f23b9258fab2829990fe7d044fa508f
David Menache <dave@doap.com>
2025-01-02 06:51:05 -0600

    working well

--- simple.php
+++ simple.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php include 'inc/php/head.php'; ?>
-    </head>
+    <head> <?php include 'inc/php/head.php'; ?> </head>
     <body>
         <div class="page-wrapper">
             <div class="main-content">
                 <?php include 'inc/html/header.html'; ?>
-
-
                 <?php include 'inc/php/shop.php'; ?>
-
-
             </div>
             <?php include 'inc/html/footer.html'; ?>
         </div>
