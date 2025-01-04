d7c86751da8e124c0a75c05bb8ca17d398328e17
David Menache <dave@doap.com>
2025-01-03 22:20:35 -0600

    Deploy head.php to the cloud.

--- inc/php/head.php
+++ inc/php/head.php
@@ -1,5 +1,6 @@
 <title>Doap</title>
 
+    <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-PHZXR9R"></script>
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
