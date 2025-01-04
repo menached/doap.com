7ebe6b944277d3dd146ae92bcedd85cb438e0b82
David Menache <dave@doap.com>
2025-01-03 22:24:54 -0600

    Deploy head.php to the cloud.

--- inc/php/head.php
+++ inc/php/head.php
@@ -1,13 +1,22 @@
 <title>Doap</title>
-
-    <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-PHZXR9R"></script>
     <!-- Google Tag Manager -->
+    <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-PHZXR9R"></script>
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-PHZXR9R');</script>
     <!-- End Google Tag Manager -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP7XRHB9TQ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-VP7XRHB9TQ');
+    </script>
 
     <script>
         const hostname = window.location.hostname;
