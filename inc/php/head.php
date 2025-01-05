3caf9d5cd9044eee3bc58200d79b04da5100f26e
David Menache <dave@doap.com>
2025-01-05 12:48:28 -0600

    Deploy head.php to the cloud.

--- inc/php/head.php
+++ inc/php/head.php
@@ -27,6 +27,7 @@
     <link rel="stylesheet" href="inc/css/style.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="inc/js/shop.js" type="module" defer></script>
+    <script src="inc/js/payments.js" type="module" defer></script>
     <script src="inc/js/subdomainData.js" type="module" defer></script>
     <script src="inc/js/shopzipFinder.js" defer></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
